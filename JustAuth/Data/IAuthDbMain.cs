3b2e343d202be8ebd6d3530592f60cd89da56014
Vansh0t <hunterzerorus@gmail.com>
2022-05-13 23:37:25 +0300

    jwt as cookie and refresh tokens

--- JustAuth/Data/IAuthDbMain.cs
+++ JustAuth/Data/IAuthDbMain.cs
@@ -4,5 +4,6 @@ namespace JustAuth.Data
     public interface IAuthDbMain<TUser> where TUser : AppUser
     {
         DbSet<TUser> Users {get;set;}
+        DbSet<JwtRefreshToken> RefreshTokens {get;set;}
     }
 }
